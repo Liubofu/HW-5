89211f52b641ef4d2a7997e1a760850bdc190b12
Liubofu <luboff1992@gmail.com>
2025-11-22 16:11:51 +0300

    HW-5

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.security.jgss.GSSUtil;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -24,6 +26,47 @@ public class Main {
             System.out.println(year + " год является високосным.");
         } else {
             System.out.println(year + " год не является високосным.");
+        }
+
+        System.out.println("Задание 4");
+
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Для доставки потребуется 1 день.");
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Для доставки потребуется 2 дня.");
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("Для доставки потребуется 3 дня.");
+        } else if (deliveryDistance > 100) {
+            System.out.println("Доставки нет.");
+        }
+
+        System.out.println("Задание 5");
+
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
         }
     }
 }
